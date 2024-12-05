7158a30c8a0bf4fdcf814abd7a9d283130c9aee1
codeclass-pc-6 <matvejv998@gmail.com>
2024-12-05 18:16:08 +0300

    хз, забейте

---
+++ hand_tests.py
@@ -0,0 +1,22 @@
+from functions import salary,hello_who
+print(hello_who("su-152"))
+if hello_who("su-152") != "hello, su-152":
+    print("капут")
+else:
+    print("зер гуд")
+
+
+#################################################
+
+if salary(4,100) != 800:
+    print("капут")
+else:
+    print("зер гуд")
+
+
+if salary(40, 6) != 480:
+    print("капут")
+else:
+    print("зер гуд")
+
+
---
+++ simple_autotests.py
@@ -0,0 +1,5 @@
+from functions import salary,hello_who
+
+#assert hello_who("su-152") == "hello,yuyuyuy", "bag"
+
+assert salary(4,100) == 200,"salary error"
